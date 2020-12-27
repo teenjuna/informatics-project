57cdb7a0a7124cb25de0c41701bcd8f0e65357b7
Alex Ryapolov <ryapolov@pm.me>
2020-12-27 23:53:52 +0700

    Simplify server logic

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,52 +1,73 @@
-use actix_web::{web, App, HttpServer};
-use std::io;
+use fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::{fs, io};
+use warp::http::{HeaderValue, Response, StatusCode as Code};
+use warp::hyper::header::CONTENT_TYPE;
+use warp::{reply, Filter, Reply};
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    println!("Running server at http://localhost:8080");
-    HttpServer::new(move || {
-        App::new().service(
-            web::scope("/paintings/{id}")
-                .service(web::resource("").route(web::get().to(handlers::get_info)))
-                .service(web::resource("/image").route(web::get().to(handlers::get_image))),
-        )
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+#[tokio::main]
+async fn main() {
+    let get_painting_info = warp::path!("painting" / u16 / "info")
+        .and(warp::get())
+        .map(|id| {
+            println!("-> Got request for info about painting {}", id);
+            let path = Path::new("data").join(format!("{}.txt", id));
+            match fs::read_to_string(path) {
+                Ok(info) => reply::with_status(info, Code::OK),
+                Err(err) => match err.kind() {
+                    io::ErrorKind::NotFound => reply::with_status(
+                        format!("info for painting {} is not found", id),
+                        Code::NOT_FOUND,
+                    ),
+                    _ => reply::with_status(
+                        format!("internal server error: {}", err),
+                        Code::INTERNAL_SERVER_ERROR,
+                    ),
+                },
+            }
+        });
+    let get_painting_image = warp::path!("painting" / u16 / "image")
+        .and(warp::get())
+        .map(|id| {
+            println!("-> Got request for image of painting {}", id);
+            let path = Path::new("data").join(format!("{}.jpg", id));
+            match File::open(path) {
+                Ok(file) => Image(file).into_response(),
+                Err(err) => match err.kind() {
+                    io::ErrorKind::NotFound => reply::with_status(
+                        format!("image for painting {} is not found", id),
+                        Code::NOT_FOUND,
+                    ),
+                    _ => reply::with_status(
+                        format!("internal server error: {}", err),
+                        Code::INTERNAL_SERVER_ERROR,
+                    ),
+                }
+                .into_response(),
+            }
+        });
+
+    println!("Starting server on http://127.0.0.1:8080");
+    let routes = get_painting_info.or(get_painting_image);
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
 
-mod handlers {
-    use actix_files::NamedFile;
-    use actix_web::{error, web, Error as HttpError, HttpResponse, Responder};
-    use server::{Error, Painting};
+struct Image(File);
 
-    pub async fn get_info(id: web::Path<u16>) -> impl Responder {
-        let id = id.into_inner();
-        println!("trying to get info {}", id);
-
-        match Painting::load_info(id) {
-            Ok(info) => HttpResponse::Ok().json(info),
-            Err(err) => match err {
-                Error::InfoNotFound(_) => error::ErrorNotFound(err).into(),
-                _ => error::ErrorInternalServerError(err).into(),
-            },
-        }
-    }
-
-    pub async fn get_image(id: web::Path<u16>) -> Result<NamedFile, HttpError> {
-        let id = id.into_inner();
-        println!("trying to get image {}", id);
-
-        match Painting::image_path(id) {
-            Ok(path) => match NamedFile::open(path) {
-                Ok(file) => Ok(file),
-                Err(err) => Err(error::ErrorInternalServerError(err)),
-            },
-            Err(err) => match err {
-                Error::ImageNotFound(_) => Err(error::ErrorNotFound(err)),
-                _ => Err(error::ErrorInternalServerError(err)),
-            },
+impl Reply for Image {
+    #[inline]
+    fn into_response(mut self) -> warp::reply::Response {
+        let mut data: Vec<u8> = Vec::new();
+        match self.0.read_to_end(&mut data) {
+            Ok(_) => Response::builder()
+                .header(CONTENT_TYPE, HeaderValue::from_static("image/png"))
+                .body(data.into())
+                .unwrap(),
+            Err(err) => Response::builder()
+                .status(Code::INTERNAL_SERVER_ERROR)
+                .body(err.to_string().into())
+                .unwrap(),
         }
     }
 }
